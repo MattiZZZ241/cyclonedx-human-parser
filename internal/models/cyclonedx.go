363a068d01f678ce6eacac0b0830890c37955189
MattiZZZ241 <matthis2401@gmail.com>
2025-01-29 20:52:40 +0100

    structure models for cdx 1.6

--- internal/models/cyclonedx.go
+++ internal/models/cyclonedx.go
@@ -1,10 +1,28 @@
 package models
 
-type Entity struct {
+type Address struct {
+	BomRef              string `json:"bom-ref,omitempty"`
+	Country             string `json:"country,omitempty"`
+	Region              string `json:"region,omitempty"`
+	Locality            string `json:"locality,omitempty"`
+	PostOfficeBoxNumber string `json:"postOfficeBoxNumber,omitempty"`
+	PostalCode          string `json:"postalCode,omitempty"`
+	StreetAddress       string `json:"streetAddress,omitempty"`
+}
+
+type Contact struct {
 	BomRef string `json:"bom-ref,omitempty"`
 	Name   string `json:"name,omitempty"`
 	Email  string `json:"email,omitempty"`
 	Phone  string `json:"phone,omitempty"`
+}
+
+type Supplier struct {
+	BomRef  string   `json:"bom-ref,omitempty"`
+	Name    string   `json:"name,omitempty"`
+	Address Address  `json:"address,omitempty"`
+	Url     []string `json:"url,omitempty"`
+	Contact Contact  `json:"contact,omitempty"`
 }
 
 type Hash struct {
@@ -34,24 +52,26 @@ type Property struct {
 }
 
 type Component struct {
-	Type        string `json:"type"`
-	MimeType    string `json:"mime-type,omitempty"`
-	BomRef      string `json:"bom-ref,omitempty"`
-	Supplier    Entity `json:"supplier,omitempty"`
-	Author      string `json:"author,omitempty"`
-	Publisher   string `json:"publisher,omitempty"`
-	Group       string `json:"group,omitempty"`
-	Name        string `json:"name"`
-	Version     string `json:"version,omitempty"`
-	Description string `json:"description,omitempty"`
-	Scope       string `json:"scope,omitempty"`
-	Hashes      []Hash `json:"hashes,omitempty"`
+	Type     string   `json:"type"`
+	MimeType string   `json:"mime-type,omitempty"`
+	BomRef   string   `json:"bom-ref,omitempty"`
+	Supplier Supplier `json:"supplier,omitempty"`
+	// Manufacturer Manufacturer `json:"manufacturer,omitempty"`
+	Authors     []Contact `json:"authors,omitempty"`
+	Publisher   string    `json:"publisher,omitempty"`
+	Group       string    `json:"group,omitempty"`
+	Name        string    `json:"name"`
+	Version     string    `json:"version,omitempty"`
+	Description string    `json:"description,omitempty"`
+	// Scope       string    `json:"scope,omitempty"` TODO: Default value
+	Hashes []Hash `json:"hashes,omitempty"`
 	// Licenses []License `json:"licenses,omitempty"` TODO: Implement dynamic parse logic
-	Copyright string `json:"copyright,omitempty"`
-	Cpe       string `json:"cpe,omitempty"`
-	Purl      string `json:"purl,omitempty"`
-	Swid      Swid   `json:"swid,omitempty"`
-	Modified  string `json:"modified,omitempty"` // Deprecated use Pedigree instead
+	Copyright string   `json:"copyright,omitempty"`
+	Cpe       string   `json:"cpe,omitempty"`
+	Purl      string   `json:"purl,omitempty"`
+	Omniborld []string `json:"omniborld,omitempty"`
+	Swhid     []string `json:"swhid,omitempty"`
+	Swid      Swid     `json:"swid,omitempty"`
 	// Pedigree  Pedigree `json:"pedigree,omitempty"` TODO
 	// ExternalReferences []ExternalReferences `json:"externalReferences,omitempty"` TODO
 	Components []Component `json:"components,omitempty"`
@@ -59,28 +79,23 @@ type Component struct {
 	// ReleaseNotes ReleaseNotes `json:"releaseNotes,omitempty"` TODO
 	// ModelCard ModelCard `json:"modelCard,omitempty"` TODO
 	// Data Data `json:"data,omitempty"` TODO
+	// CryptoProperties CryptoProperties `json:"cryptoProperties,omitempty"`
 	Properties []Property `json:"properties,omitempty"`
+	Tags       []string   `json:"tags,omitempty"`
 	// Signature Signature `json:"signature,omitempty"` TODO
 }
 
 type Dependency struct {
 	Ref       string   `json:"ref"`
 	DependsOn []string `json:"dependsOn,omitempty"`
+	Provides  []string `json:"provides,omitempty"`
 }
 
-// type Metadata struct {
-// 	Timestamp string `json:"timestamp,omitempty"`
-// 	// Lifecycles []Lifecycle `json:"lifecycles,omitempty"` TODO: Implement dynamic parse logic
-// 	// Tools   []Tool   `json:"tool,omitempty"` TODO: Write Tools models
-// 	Authors   []Author  `json:"authors,omitempty"`
-// 	Component Component `json:"component"`
-// }
-
 type CycloneDX struct {
-	BomFormat   string `json:"bomFormat"`
-	SpecVersion string `json:"specVersion"`
-	// SerialNumber string   `json:"serialNumber,omitempty"`
-	// Version      string   `json:"version,omitempty"`
+	BomFormat    string `json:"bomFormat"`
+	SpecVersion  string `json:"specVersion"`
+	SerialNumber string `json:"serialNumber,omitempty"`
+	Version      int    `json:"version,omitempty"`
 	// Metadata     Metadata `json:"metadata,omitempty"`
 	Components []Component `json:"components,omitempty"`
 	// Services   []Service   `json:"services,omitempty"` TODO
@@ -90,6 +105,8 @@ type CycloneDX struct {
 	// Vulnerabilities []Vulnerability `json:"vulnerabilities,omitempty"` TODO
 	// Annotations []Annotation `json:"annotations,omitempty"` TODO
 	// Formulation []Formulation `json:"formulation,omitempty"` TODO
+	// Declarations Declarations `json:"declarations,omitempty"`
+	// Definitions Standards  `json:"definitions,omitempty"`
 	Properties []Property `json:"properties,omitempty"`
 	// Signature Signature `json:"signature,omitempty"` TODO
 }
